07f1bf59cc5d6b60238dc3ad8c6bc314206a70b6
Marcus Vinicius <marcusviniciusrocha97@gmail.com>
2024-11-08 15:20:45 -0300

    Entrega projeto

---
+++ scr/navegador.js
@@ -0,0 +1,8 @@
+
+let heroi = prompt("Digite seu nick")
+console.log ("Olá Herói " + heroi + "!")
+let xp = prompt(" Digite seu Nível ") 
+
+if (xp <= 1000){
+    console.log(" O Herói " + heroi + " tem " + xp + " e é nível ") 
+}
